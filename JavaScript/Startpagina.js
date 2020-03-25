03bbbf17a7619b8d95b085e386b3b3aacff7dd80
MaartenVanhaerenPXL <11800644@student.pxl.be>
2020-03-25 14:04:49 +0100

    message versturen (front-end af)

--- JavaScript/Startpagina.js
+++ JavaScript/Startpagina.js
@@ -3,6 +3,7 @@ let deleteButton = document.getElementById("Delete");
 let chatroomOf = document.getElementById("Chatroom")
 let welkom = document.getElementById("Welkom");
 let sendButton = document.getElementById("Send")
+let table = document.getElementById("messageTable");
 //hardcoded users
 let userA = "user1";
 let userB = "user2";
@@ -15,9 +16,26 @@ deleteButton.onclick = deleteFunction;
 function deleteFunction() {
     message.value = "";
 }
+function sendMessage(toAddMassage) {
+    let tr = document.createElement("tr");
+    let tdEmpty = document.createElement("td");
+    let tdMessage = document.createElement("td");
+    let messageBox = document.createElement("span");
+    messageBox.innerHTML = toAddMassage;
+    messageBox.id = "sendedMessage";
+    tdMessage.id = "messageBox";
+    tdMessage.appendChild(messageBox)
+    tr.appendChild(tdEmpty);
+    tr.appendChild(tdMessage);
+    table.appendChild(tr);
+    console.log(table);
+
+
+}
 
 sendButton.onclick = sendFunction;
 
 function sendFunction() {
     console.log(message.value);
+    sendMessage(message.value)
 }
